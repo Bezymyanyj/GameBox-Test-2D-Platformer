cd6c8319960df3f05d103ad5c707e8f40c9994b0
Bezymyanyj <Bezymyanyj@gmail.com>
2021-01-27 06:11:38 -0500

    Folder

---
+++ GameBox Test Platformer/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform target;
+
+    public float speed;
+
+    private Vector2 startPosition;
+    private Transform playerPosition;
+
+    private bool isPLayerDetected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPLayerDetected)
+        {
+            FollowToPlayer(playerPosition.position);
+            //Debug.Log($"PLayer position: {playerPosition.position}");
+        }
+        else
+        {
+            var distance = Vector2.Distance(transform.position, target.position);
+            if (distance < 0.1f)
+            {
+                var tmp = target.position;
+                target.position = startPosition;
+                startPosition = tmp;
+            }
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+        
+        
+    }
+
+    private void FollowToPlayer(Vector2 player)
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player, speed * Time.deltaTime);
+    }
+
+    public void PlayerDetected(Transform playerPosition)
+    {
+        isPLayerDetected = true;
+        this.playerPosition = playerPosition.GetComponent<Transform>();
+    }
+
+    public void PlayerRunAway()
+    {
+        isPLayerDetected = false;
+    }
+}

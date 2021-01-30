6ec40e7b19b5c6c5ce38ab6cdb4f7c7070579ac7
Bezymyanyj <Bezymyanyj@gmail.com>
2021-01-29 20:23:07 -0500

    Death

--- GameBox Test Platformer/Assets/Scripts/PlayerAnimationController.cs
+++ GameBox Test Platformer/Assets/Scripts/PlayerAnimationController.cs
@@ -2,14 +2,19 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerAnimationController : MonoBehaviour
 {
     private Animator animator;
+    private PlayerMovement movement;
+    private CameraShake cameraShake;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        movement = GetComponent<PlayerMovement>();
+        cameraShake = GetComponent<CameraShake>();
     }
 
     public void Jump()
@@ -20,5 +25,20 @@ public class PlayerAnimationController : MonoBehaviour
     public void Run(bool isRunning)
     {
         animator.SetBool("Run", isRunning);
+    }
+
+    public void Death()
+    {
+        movement.enabled = false;
+        animator.SetTrigger("Death");
+        var timeDelay = animator.GetCurrentAnimatorStateInfo(0).length;
+        StartCoroutine(PlayAgain(timeDelay));
+        StartCoroutine(cameraShake.ShakeCamera(timeDelay));
+    }
+
+    private IEnumerator PlayAgain(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene("Well");
     }
 }
--- GameBox Test Platformer/Assets/Scripts/SpikeCollisionChecker.cs
+++ GameBox Test Platformer/Assets/Scripts/SpikeCollisionChecker.cs
@@ -8,7 +8,9 @@ public class SpikeCollisionChecker : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("Player"))
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<PlayerAnimationController>().Death();
+        }
     }
 }
